c6a59484df08da4da685b4dd5886048e125ef3a3
fadillahcheryl <fadillahcheryl88@gmail.com>
2023-05-25 22:59:53 +0800

    Add Scene

--- src/main/java/com/example/mainproject/HelloApplication.java
+++ src/main/java/com/example/mainproject/HelloApplication.java
@@ -2,9 +2,12 @@ package com.example.mainproject;
 
 import javafx.application.Application;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.ScrollPane;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.VBox;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 public class HelloApplication extends Application {
@@ -35,15 +38,33 @@ public class HelloApplication extends Application {
     }
     public static void regNameTB(){
         System.out.println("Nama, Umur, TB, BB");
+        VBox1.addall(lNU, tfNama, tfUmur, btnNU);
+        Label lNU = new Label("Nama dan Umur");
+        TextField tfNama = new TextField();
+        tfNama.setPromptText("Nama");
+        TextField tfUmur = new TextField();
+        tfNama.setPromptText("Umur");
+        Button btnNU = new Button("Lanjutkan");
     }
     public static void regTekananDarah(){
         System.out.println("Tekanan Darah");
+        VBox2.addall(lTekDar, tfTekDar, btnTekDar);
+        Label lTekDar = new Label("Tekanan Darah");
+        TextField tfTekDar = new TextField();
+        Button btnTekDar = new Button("Lanjutkan");
     }
     public static void regGulaDarah(){
         System.out.println("Gula Darah");
+        VBox3.addall(lGulDar, tfGulDar, btnGulDar);
+        Label lGulDar = new Label("Gula Darah");
+        TextField tfGulDar = new TextField();
+        Button btnGulDar = new Button("Lanjutkan");
     }
     public static void regKonsumsi(){
         System.out.println("Konsumsi Air");
+        VBox4.addall(btnMinum, btnKA);
+        Button btnMinum = new Button("Minum");
+        Button btnKA = new Button("Lanjutkan");
     }
     public static void dashboard(){
         System.out.println("DashBoard");
